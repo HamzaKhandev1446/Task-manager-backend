674633ba9bd4e1dc772884a3ecf94a27eb14dfac
Hamza Khan <hamzakhan.dev1446@gmail.com>
2024-08-19 17:13:27 +0500

    Added server configs.

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcvoome to the Task Manager API!');
+});
+
+module.exports = app;
